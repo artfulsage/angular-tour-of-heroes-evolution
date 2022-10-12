c12fb662555b993a2e28457b0d84e22f912d1ff1
artfulsage <artful.sage@yahoo.com>
2022-10-12 22:27:22 +0300

    improve debounce pipe

--- src/app/common/pipes/debounce.pipe.ts
+++ src/app/common/pipes/debounce.pipe.ts
@@ -1,16 +1,48 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Observable, timer } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ChangeDetectorRef, NgZone, Pipe, PipeTransform } from '@angular/core';
+
+const NOT_SET_PREVIOUS_VALUE = Symbol('NOT_SET_PREVIOUS_VALUE');
+const NOT_SET_NEXT_VALUE = Symbol('NOT_SET_NEXT_VALUE');
 
 @Pipe({
-  name: 'debounce'
+  name: 'debounce',
+  pure: false,
 })
 export class DebouncePipe implements PipeTransform {
-  prevValue: unknown;
+  private previousValue: any = NOT_SET_PREVIOUS_VALUE;
+  private nextValue: any = NOT_SET_NEXT_VALUE;
+  private debounceTimer: any;
 
-  transform<T>(value: T, debounce: number): Observable<T> {
-    return timer(debounce).pipe(
-      map(() => value),
-    );
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private readonly zone: NgZone,
+  ) {}
+
+  transform<T>(currentValue: T, debounceTime: number): T {
+    if (this.previousValue === NOT_SET_PREVIOUS_VALUE) {
+      this.previousValue = currentValue;
+      return currentValue;
+    }
+
+    if (this.previousValue === currentValue) {
+      clearTimeout(this.debounceTimer);
+      return currentValue;
+    }
+
+    if (this.nextValue !== currentValue) {
+      this.nextValue = currentValue;
+
+      clearTimeout(this.debounceTimer);
+
+      this.debounceTimer = setTimeout(() => {
+        this.zone.run(() => {
+          this.debounceTimer = undefined;
+          this.previousValue = currentValue;
+          this.nextValue = NOT_SET_NEXT_VALUE;
+          this.changeDetector.markForCheck();
+        });
+      }, debounceTime);
+    }
+
+    return this.previousValue;
   }
 }
